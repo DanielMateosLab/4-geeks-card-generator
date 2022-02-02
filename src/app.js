c15c64d11dff4713f3f7d8b56b1a2d937ae60450
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-02 20:08:54 +0000

    Add interactive shuffling

--- src/app.js
+++ src/app.js
@@ -3,5 +3,30 @@ import "bootstrap";
 import "./style.css";
 
 const CARD_SIZE_RATIO = 0.7143;
+const CARD_VALUES = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const CARD_SUITS = [
+  { color: "red", suit: "♥" },
+  { color: "red", suit: "♦" },
+  { color: "black", suit: "♣" },
+  { color: "black", suit: "♠" }
+];
 
-window.onload = function() {};
+window.onload = function() {
+  shuffleCards();
+};
+
+function pickRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function shuffleCards() {
+  const value = pickRandom(CARD_VALUES);
+  const { color, suit } = pickRandom(CARD_SUITS);
+
+  document.querySelectorAll(".suit").forEach(element => {
+    element.style.color = color;
+    element.innerHTML = suit;
+  });
+
+  document.querySelector(".value").innerHTML = value;
+}
